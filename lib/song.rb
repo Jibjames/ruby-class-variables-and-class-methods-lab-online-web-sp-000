2bf2ad5652ac1dbdebdf9c7a56383193a189fd12
James Bridges <masterbridges@gmail.com>
2020-01-06 00:42:54 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+
+  @@count = 0
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+  end
+
+  def count()
+    @@count
+  end
+
+  def genres()
+
+  end
+
+
+end
